dca8b2c049463f56db1e164b47abd50518cd21e9
Raimon Esteve <raimon@nan-tic.com>
2017-04-07 13:34:18 +0200

    FIX test

--- tests/test_stock_catalogue.py
+++ tests/test_stock_catalogue.py
@@ -1,33 +1,18 @@
-# The COPYRIGHT file at the top level of this repository contains the full
-# copyright notices and license terms.
+# This file is part of Tryton.  The COPYRIGHT file at the top level of
+# this repository contains the full copyright notices and license terms.
 import unittest
-# import doctest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
-# TODO: Remove if no sceneario needed.
-# from trytond.tests.test_tryton import doctest_setup, doctest_teardown
+from trytond.tests.test_tryton import ModuleTestCase, with_transaction
+from trytond.pool import Pool
 
 
-class TestCase(unittest.TestCase):
-    'Test module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('stock_catalogue')
-
-    def test0005views(self):
-        'Test views'
-        test_view('stock_catalogue')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+class StockCatalogueTestCase(ModuleTestCase):
+    'Test Stock Catalogue module'
+    module = 'stock_catalogue'
 
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
-    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(TestCase))
-    # TODO: remove if no scenario needed.
-    #suite.addTests(doctest.DocFileSuite('scenario_stock_catalogue.rst',
-    #        setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
-    #        optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
+    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
+        StockCatalogueTestCase))
     return suite
