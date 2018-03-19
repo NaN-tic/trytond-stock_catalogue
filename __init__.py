21e5eea3a29a56df12c20154d0332aa5035bcdff
Raimon Esteve <raimon@nan-tic.com>
2018-03-19 14:07:34 +0100

    Upgrade from 4.0 series

--- __init__.py
+++ __init__.py
@@ -1,8 +1,8 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 from trytond.pool import Pool
-import location
-import user
+from . import location
+from . import user
 
 
 def register():
@@ -10,4 +10,5 @@ def register():
         location.Catalogue,
         location.CatalogueLines,
         user.User,
+        user.UserCatalogue,
         module='stock_catalogue', type_='model')
--- user.py
+++ user.py
@@ -1,20 +1,41 @@
 # This file is part of Tryton.  The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
-from trytond.model import fields
+from trytond.model import ModelSQL, ModelView, fields
 from trytond.pool import PoolMeta
 from trytond.pyson import Eval, Bool
 
-__all__ = ['User']
-__metaclass__ = PoolMeta
+__all__ = ['User', 'UserCatalogue']
 
 
 class User:
+    __metaclass__ = PoolMeta
     __name__ = 'res.user'
-
-    catalogue = fields.Many2One('stock.location.catalogue', 'Catalogue')
+    from_location = fields.Many2One('stock.location', 'From Location',
+        domain=[
+            ('type', 'in', ['view', 'storage']),
+        ], states={
+            'required': Bool(Eval('catalogue')),
+        })
     location = fields.Many2One('stock.location', 'Location',
         domain=[
             ('type', '=', 'storage'),
         ], states={
             'required': Bool(Eval('catalogue')),
         })
+    catalogues = fields.Many2Many('res.user-stock.location.catalogue', 'user',
+        'catalogue', 'Catalogues')
+
+    @classmethod
+    def __setup__(cls):
+        super(User, cls).__setup__()
+        cls._context_fields.insert(0, 'catalogues')
+
+
+class UserCatalogue(ModelSQL, ModelView):
+    'User - Catalogue'
+    __name__ = 'res.user-stock.location.catalogue'
+
+    user = fields.Many2One('res.user', 'User', ondelete='CASCADE',
+        required=True, select=True)
+    catalogue = fields.Many2One('stock.location.catalogue', 'Catalogue',
+        ondelete='CASCADE', required=True, select=True)
