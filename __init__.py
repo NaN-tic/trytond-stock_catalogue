46064b6a06025233d8877e075f666e97ca823321
Raimon Esteve <raimon@nan-tic.com>
2018-10-16 11:57:28 +0200

    Review and rename user context/fields

--- __init__.py
+++ __init__.py
@@ -1,14 +1,13 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 from trytond.pool import Pool
-from . import location
+from . import catalogue
 from . import user
-
 
 def register():
     Pool.register(
-        location.Catalogue,
-        location.CatalogueLines,
+        catalogue.Catalogue,
+        catalogue.CatalogueLine,
         user.User,
         user.UserCatalogue,
         module='stock_catalogue', type_='model')
